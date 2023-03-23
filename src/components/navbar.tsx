cb9b7b693bb5f13e0c0335523d48f842bbb5d3d5
AdiH16 <adi.hinovic.22@size.ba>
2023-03-23 01:10:23 +0100

    Account settings

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -201,14 +201,14 @@ const Navbar = (props: NavbarProps) => {
                         <span className="ml-2">{props.username}</span> <svg className="w-5 h-5 ml-1" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd"></path></svg></button>
                         <div id="dropdownNavbar" className={` z-10 ${profileDropdownOpen ? "block" : "hidden"} details mt-2 font-normal bg-white divide-y divide-gray-100 rounded-lg shadow lg:w-36 dark:bg-gray-700 dark:divide-gray-600 lg:absolute`}>
                             <ul className="py-2 text-sm text-gray-700 dark:text-gray-400" aria-labelledby="dropdownLargeButton">
-                              <li>
+                              {/*<li>
                                 <Link href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white flex items-center space-x-2">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                                   <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
                                 </svg>
                                 <div>Profil</div>
                                 </Link>
-                              </li>
+                              </li>*/}
                               <li>
                                 <Link href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white flex items-center space-x-2">
                                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
